9d2e664ee9b284e0ebe9a8459eac912c46e98f2e
Omega Dziko F. <megaoy@yahoo.com>
2024-04-29 21:19:28 -0400

    built the class and attempted a test

--- employee.js
+++ employee.js
@@ -1,1 +1,24 @@
-// Your code here 
+class Employee {
+
+    constructor(name, occupation) {
+
+        this.name = name;
+
+        this.occupation = occupation;
+
+    }
+
+    sayName() {
+
+        console.log(`${name} says hello`);
+
+    }
+
+    sayOccupation() {
+
+        console.log(`${name} is a ${this.occupation}`);
+
+    }
+}
+
+module.exports = Employee;
--- test.js
+++ test.js
@@ -1,1 +1,8 @@
-// Your code here 
+const Employee = require('./employee');
+
+const newEmployee = new Employee("John Wick", "Dog Lover");
+
+
+setTimeout(() => {
+    newEmployee.sayName(); // This will be called after 2 seconds
+  }, 2000);
